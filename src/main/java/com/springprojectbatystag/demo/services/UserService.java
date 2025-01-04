74d9a05d176054f2b6158e2cc9e73dd71126e272
Abner Garbin <abner.batysta151@gmail.com>
2025-01-04 13:56:02 -0300

    Exception Update

--- src/main/java/com/springprojectbatystag/demo/services/UserService.java
+++ src/main/java/com/springprojectbatystag/demo/services/UserService.java
@@ -4,6 +4,7 @@ import com.springprojectbatystag.demo.entities.User;
 import com.springprojectbatystag.demo.repositories.UserRepository;
 import com.springprojectbatystag.demo.services.exceptions.DatabaseException;
 import com.springprojectbatystag.demo.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -48,9 +49,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
